912d1fa65970b584e71b2eb494b4924f13a07f5c
Manuel Osorio Wallace <mosoriow@hotmail.com>
2021-01-08 13:50:40 +0100

    Create ProductCategory and start building it's controller.

---
+++ LexShop.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using LexShop.Core.Models;
+
+namespace LexShop.DataAccess.InMemory
+{
+    public class ProductCategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<ProductCategory> productCategories = new List<ProductCategory>();
+
+        public ProductCategoryRepository()
+        {
+            productCategories = cache["productsCategory"] as List<ProductCategory>;
+            if (productCategories == null)
+            {
+                productCategories = new List<ProductCategory>();
+            }
+
+        }
+        public void Commit()
+        {
+            cache["productCategory"] = productCategories;
+        }
+        public void Insert(ProductCategory productCategory)
+        {
+            productCategories.Add(productCategory);
+        }
+        public void Update(ProductCategory productCategory)
+        {
+            ProductCategory productCategoryToUpdate = productCategories.Find(p => p.ID == productCategory.ID);
+            if (productCategoryToUpdate != null)
+            {
+                productCategoryToUpdate = productCategory;
+            }
+            else
+            {
+                throw new Exception("Product Category not found");
+            }
+        }
+        public ProductCategory Find(string ID)
+        {
+            ProductCategory productCategory = productCategories.Find(p => p.ID == ID);
+            if (productCategory != null)
+            {
+                return productCategory;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+        public IQueryable<ProductCategory> Collection()
+        {
+            return productCategories.AsQueryable();
+        }
+        public void Delete(string ID)
+        {
+            ProductCategory productCategoryToDelete = productCategories.Find(p => p.ID == ID);
+            if (productCategoryToDelete != null)
+            {
+                productCategories.Remove(productCategoryToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+    }
+}
---
+++ LexShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LexShop.Core.Models;
+using LexShop.DataAccess.InMemory;
+
+namespace LexShop.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        ProductCategoryRepository context;
+        public ProductCategoryManagerController()
+        {
+            context = new ProductCategoryRepository();
+
+        }
+        public ActionResult Index()
+        {
+            List<ProductCategory> productCategories = context.Collection().ToList();
+            return View(productCategories);
+
+        }
+
+        public ActionResult Create()
+        {
+            ProductCategory productCategory = new ProductCategory();
+
+            return View(productCategory);
+        }
+        public ActionResult Create(ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCategory);
+            }
+            else
+            {
+                context.Insert(productCategory);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string ID)
+        {
+            ProductCategory productCategory = context.Find(ID);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productCategory);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productCategory, string ID)
+        {
+            ProductCategory productCategoryToEdit = context.Find(ID);
+            if (productCategoryToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if(!ModelState.IsValid)
+                {
+                    return View(productCategory);
+                }
+                else
+                {
+                    productCategoryToEdit.Category = productCategory.Category;
+                    context.Commit();
+                    return Redirect("Index");
+                }
+            }
+        }
+
+    }
+}

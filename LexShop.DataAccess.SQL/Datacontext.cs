409ab31ad18f872cccf90310947bb9fba05087f1
Manuel Osorio Wallace <mosoriow@hotmail.com>
2021-01-12 13:40:29 +0100

    DB connection

---
+++ LexShop.DataAccess.SQL/Datacontext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using LexShop.Core.Models;
+
+namespace LexShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}

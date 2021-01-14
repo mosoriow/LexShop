10fd77e62efc40b2da6ecb7a418b263a71159862
Manuel Osorio Wallace <mosoriow@hotmail.com>
2021-01-14 16:50:47 +0100

    first aprox. to having a basket

---
+++ LexShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,25 @@
+﻿using LexShop.Core.Contracts;
+using LexShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LexShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IRepository<Product> context;
+        public BasketController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+        }
+
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+    }
+}

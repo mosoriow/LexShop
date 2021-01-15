e2d69aa8d08c02795b1c3fdd8abdd1d0859e3057
Manuel Osorio Wallace <mosoriow@hotmail.com>
2021-01-15 09:33:57 +0100

    Add Basket and BasketItem Models

---
+++ LexShop.DataAccess.SQL/Migrations/202101150830035_AddBasket.cs
@@ -0,0 +1,42 @@
+namespace LexShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBasket : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BasketItems",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 128),
+                        BasketID = c.String(maxLength: 128),
+                        ProductID = c.String(),
+                        CreateAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Baskets", t => t.BasketID)
+                .Index(t => t.BasketID);
+            
+            CreateTable(
+                "dbo.Baskets",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 128),
+                        CreateAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.BasketItems", "BasketID", "dbo.Baskets");
+            DropIndex("dbo.BasketItems", new[] { "BasketID" });
+            DropTable("dbo.Baskets");
+            DropTable("dbo.BasketItems");
+        }
+    }
+}
---
+++ LexShopCore/Models/BasketItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexShop.Core.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        public string BasketID { get; set; }
+        public string ProductID { get; set; }
+        public uint Quantity { get; set; }
+    }
+}
